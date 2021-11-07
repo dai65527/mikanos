2fbf1d44a662baf84ba5d9abefde9b27239e595f
dnakano <dnakano@student.42tokyo.jp>
2021-11-07 12:14:40 +0900

    hello world worked

--- hello/create-diskimg.sh
+++ hello/create-diskimg.sh
@@ -36,3 +36,23 @@ qemu-img create -f raw disk.img 200M
 #  - FAT: File Allocation Table (FAT) はコンピュータファイルシステムの構造であり、その構造を利用する業界標準のファイルシステムを指す
 #  - USBメモリの代わりにディスクイメージを作ってフォーマットするようなイメージかな？
 mkfs.fat -n 'MIKAN OS' -s 2 -f 2 -R 32 -F 32 disk.img
+
+# mntディレクトリを作成(disk.imgのマウント先)
+mkdir -p mnt
+
+# disk.imgを./mntにディレクトリにマウントする
+# Arguments
+#   - -o loop: ループバックマウントするためのオプション
+#              https://oplern.hatenablog.com/entry/2017/06/30/231027
+#              https://qiita.com/aki3061/items/55e0061e6d5fca78631f#%E3%83%AB%E3%83%BC%E3%83%97%E3%83%90%E3%83%83%E3%82%AF%E3%83%87%E3%83%90%E3%82%A4%E3%82%B9%E3%81%AE%E6%B4%BB%E7%94%A8
+#   - disk.img: ディスクイメージのパス
+#   - mnt: マウント先のパス
+sudo mount -o loop disk.img mnt
+
+# マウントしたdisk.imgにバイナリを書き込む
+# mnt/EFI/BOOT/BOOTX64.EFI
+sudo mkdir -p mnt/EFI/BOOT
+sudo cp hello.efi mnt/EFI/BOOT/BOOTX64.EFI
+
+# アンマウント
+sudo umount mnt
